18973dde19912387d22dc53d9affd200e88e0e91
John Readey <jreadey@hdfgroup.org>
2018-09-28 12:27:52 -0700

    added retry logic

--- tenx/hs_get_sums.py
+++ tenx/hs_get_sums.py
@@ -1,10 +1,24 @@
 import h5pyd as h5py
 import numpy as np
+import time
+
+max_retry = 60
 
 with h5py.File("/shared/bioconductor/tenx_full.h5", "r") as f:
     dset = f["newassay001"]
     num_col = dset.shape[0]
     for i in range(0, num_col):
-        arr = dset[i,:]
+        # TBD: Move this retry logic into h5pyd
+        retry_count = 0
+        while True:
+            try:
+                arr = dset[i,:]
+                break
+            except OSError as oe:
+                retry_count += 1
+                print("ERROR[{}]: {}".format(retry_count, oe))
+                time.sleep(1)
+
+
         sum = np.sum(arr)
         print(sum)
